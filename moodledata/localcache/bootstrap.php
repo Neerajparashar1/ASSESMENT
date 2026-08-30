fb74f5f326f646fc0c17d5bd09b10cf1e1af294b
Neerajparashar1 <np5668342@gmail.com>
2026-08-30 20:06:17 +0530

    Remove moodledata from repo (Moodle runtime cache/sessions, not source)

--- moodledata/localcache/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-// ********** This file is generated DO NOT EDIT **********
-$CFG->siteidentifier = 'O0qoM2P1nmAoRnt0P1R1vVcJyBzjknNhlocalhost';
-$CFG->bootstraphash = '3bffed759fdae0a1c2cf0822c25d0026';
-// Only if the file is not stale and has not been defined.
-if ($CFG->bootstraphash === hash_local_config_cache() && !defined('SYSCONTEXTID')) {
-    define('SYSCONTEXTID', 1);
-}
